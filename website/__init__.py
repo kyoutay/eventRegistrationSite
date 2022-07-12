bf0afc9e62de30020c511d925be1930361bd7223
Kyle Thai <63701122+kyoutay@users.noreply.github.com>
2022-07-12 12:32:10 -0700

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -5,17 +5,17 @@ mail = Mail()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'thunderNuggies reign supreme over seismic toss mosses'
+    app.config['SECRET_KEY'] = ''
     
     app.config['DEBUG'] = True #false for production
     app.config['TESTING'] = True #false for production
-    app.config['MAIL_SERVER'] = 'smtp.zoho.com' #an email service that allows less secure access(gmail doesnt support anymore)
-    app.config['MAIL_PORT'] = '587' #email port for mail server
-    app.config['MAIL_USERNAME'] = 'extremetossdomination@zohomail.com' #email address
-    app.config['MAIL_PASSWORD'] = 'P@ss5039!' #email pw
+    app.config['MAIL_SERVER'] = '' #an email service that allows less secure access(gmail doesnt support anymore)
+    app.config['MAIL_PORT'] = '' #email port for mail server
+    app.config['MAIL_USERNAME'] = ' #email address
+    app.config['MAIL_PASSWORD'] = '' #email pw
     app.config['MAIL_USE_TLS'] = True #depending on mail server
     app.config['MAIL_USE_SSL'] = False #depending on mail server
-    app.config['MAIL_DEFAULT_SENDER'] = 'extremetossdomination@zohomail.com' 
+    app.config['MAIL_DEFAULT_SENDER'] = '' 
     
     mail.init_app(app)
 
